5f287e9ba812c7274ddb6c5ea1aca07ead946cc2
andrealouisestanderen <andreastanderen@gmail.com>
2021-05-03 18:18:15 +0100

    Add upload, dropdown menus and scatterplot

--- analysis.py
+++ analysis.py
@@ -6,21 +6,40 @@ import os
 import colors
 import time
 
-def barChart(file, column, sort_by_count):
-    df = pd.read_csv(file)
-    sort = sort_by_count=="1"
+
+def getColumns(file):
+    df = pd.read_csv('./files/' + file)
+    columns = df.columns.tolist()
+    logic_columns = []
+    for column in columns:
+        if ' ' not in column:
+            logic_columns.append(column)
+
+    return logic_columns
+
+
+def barChart(file, column, sort_by_count, bins):
+    df = pd.read_csv('./files/' + file)
+    sort = sort_by_count == "1"
     df_parsed = df[[column]]
     values = df_parsed[column].value_counts(sort=sort).index.tolist()
     counts = df_parsed[column].value_counts(sort=sort).tolist()
-    
+    if bins == 'head':
+        values = df_parsed[column].value_counts(sort=sort).index.tolist()[:10]
+        counts = df_parsed[column].value_counts(sort=sort).tolist()[:10]
+
     # Make plot
     if sort:
-        plt.bar(range(len(values)), list(map(float, counts)), color = colors.GREEN)
+        plt.bar(range(len(values)), list(
+            map(float, counts)), color=colors.GREEN)
         plt.xticks(range(len(values)), values, rotation='vertical')
 
     else:
-        plt.bar(values, counts, color = colors.GREEN)
+        plt.bar(values, counts, color=colors.GREEN)
         plt.xticks(rotation='vertical')
+
+    plt.xlabel(column)
+    plt.ylabel('Amount')
 
     # save img
     plot_name = saveImage()
@@ -28,36 +47,55 @@ def barChart(file, column, sort_by_count):
     return plot_name
 
 
-def lineChart(file, year, group): 
+def lineChart(file, year, group):
     # in case of globalterrorism.csv, recommended groups are:
     # attacktype1_txt, targtype1_txt, region_txt, success, suicide, weaptype1_txt
     count = 0
     style = 0
-    styles = ['solid','dashed', 'dotted','dashdot']
+    styles = ['solid', 'dashed', 'dotted', 'dashdot']
     clrs = colors.COLORS
 
-    df = pd.read_csv(file)
+    df = pd.read_csv('./files/' + file)
     df_parsed = df[[year, group]]
     group_list = df_parsed[group].unique()
 
     for member in group_list:
         df_group = df_parsed.loc[df_parsed[group] == member]
 
-        values = df_group[year].value_counts().sort_index(ascending=True).index.tolist()
-        counts = df_group[year].value_counts().sort_index(ascending=True).tolist()
-        plt.plot(values, counts, color=clrs[count], label=member, linestyle=styles[style])
-        
-        if count < len(clrs) -1:
-            count+=1
+        values = df_group[year].value_counts().sort_index(
+            ascending=True).index.tolist()
+        counts = df_group[year].value_counts(
+        ).sort_index(ascending=True).tolist()
+        plt.plot(values, counts, color=clrs[count],
+                 label=member, linestyle=styles[style])
+
+        if count < len(clrs) - 1:
+            count += 1
         else:
-            style +=1
-            count=0
+            style += 1
+            count = 0
         if style == len(styles)-1:
-            style=0
+            style = 0
 
     plt.legend()
-    
+    plt.xlabel(year)
+    plt.ylabel(group)
+
     # save img
+    plot_name = saveImage()
+
+    return plot_name
+
+
+def scatterPlot(file, column1, column2):
+    df = pd.read_csv('./files/' + file)
+    x = df[[column1]]
+    y = df[[column2]]
+    plt.scatter(x, y, color=colors.GREEN)
+
+    plt.xlabel(column1)
+    plt.ylabel(column2)
+
     plot_name = saveImage()
 
     return plot_name
--- api.py
+++ api.py
@@ -1,37 +1,82 @@
-from flask import Flask, request, render_template
+import analysis
+from flask import Flask, session, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
+import json
 import matplotlib
 matplotlib.use('Agg')
-import analysis
 
 print("BAR CHART: http://127.0.0.1:5000/bar-chart?file=globalterrorism.csv&column=iyear&sort=0")
 print("LINE CHART: http://127.0.0.1:5000/line-chart?file=globalterrorism.csv&year=iyear&group=region_txt")
 
+UPLOAD_FOLDER = './files'
+ALLOWED_EXTENSIONS = set(['csv'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello():
-    return render_template('home.html')
 
-@app.route("/bar-chart")
+# make this like the image html in cloud
+@app.route("/", methods=['GET', 'POST'])
+def uploadFile():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            columns = analysis.getColumns(filename)
+            return render_template('home.html', columns=columns, filename=filename)
+
+    return render_template('upload.html')
+
+
+@ app.route("/bar-chart")
 def barChart():
 
     file = request.args.get('file')
-    column = request.args.get('column')
+    column1 = request.args.get('column1')
     sort = request.args.get('sort')
-    
-    plot_name = analysis.barChart(file, column, sort)
+    bins = request.args.get('bins')
+
+    plot_name = analysis.barChart(file, column1, sort, bins)
 
     return render_template('index.html', plot=plot_name)
 
 
-@app.route("/line-chart")
+@ app.route("/line-chart")
 def lineChart():
 
     file = request.args.get('file')
-    year = request.args.get('year')
-    group = request.args.get('group')
-    
+    year = request.args.get('column1')
+    group = request.args.get('column2')
+
     plot_name = analysis.lineChart(file, year, group)
 
     return render_template('index.html', plot=plot_name)
+
+
+@ app.route("/scatter-plot")
+def scatterPlot():
+
+    file = request.args.get('file')
+    column1 = request.args.get('column1')
+    column2 = request.args.get('column2')
+
+    plot_name = analysis.scatterPlot(file, column1, column2)
+
+    return render_template('index.html', plot=plot_name)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
